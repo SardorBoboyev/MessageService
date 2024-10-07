7877a0fcf03e6680e56b51892f163f0dceb0e681
raykhonaNorboeva <raykhonanorboeva050@gmail.com>
2024-10-07 13:49:51 +0500

    feignConfig

--- src/main/java/uz/sb/messageservice/clients/ChatServiceClient.java
+++ src/main/java/uz/sb/messageservice/clients/ChatServiceClient.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import uz.sb.messageservice.config.FeignConfig;
 import uz.sb.messageservice.domain.dto.response.ChatServiceResponse;
 
-@FeignClient(name = "CHAT-SERVICE", configuration = FeignClientsConfiguration.class)
+@FeignClient(name = "CHAT-SERVICE", configuration = FeignConfig.class)
 public interface ChatServiceClient {
 
 
