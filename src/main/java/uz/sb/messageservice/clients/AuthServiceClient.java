6739b24522d71387fcfa594e2c64fe38dd32af54
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-08 15:35:23 +0500

    Message Service changed with library part 2

--- src/main/java/uz/sb/messageservice/clients/AuthServiceClient.java
+++ src/main/java/uz/sb/messageservice/clients/AuthServiceClient.java
@@ -1,12 +1,11 @@
 package uz.sb.messageservice.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uz.sb.domain.dto.response.UserResponse;
 import uz.sb.messageservice.config.FeignConfig;
-import uz.sb.messageservice.domain.dto.response.UserResponse;
+
 
 @FeignClient(name = "AUTH-SERVICE", configuration = FeignConfig.class)
 public interface AuthServiceClient {
--- src/main/java/uz/sb/messageservice/clients/ChatServiceClient.java
+++ src/main/java/uz/sb/messageservice/clients/ChatServiceClient.java
@@ -1,12 +1,11 @@
 package uz.sb.messageservice.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uz.sb.domain.dto.response.ChatServiceResponse;
 import uz.sb.messageservice.config.FeignConfig;
-import uz.sb.messageservice.domain.dto.response.ChatServiceResponse;
+
 
 
 @FeignClient(name = "CHAT-SERVICE", configuration = FeignConfig.class)
