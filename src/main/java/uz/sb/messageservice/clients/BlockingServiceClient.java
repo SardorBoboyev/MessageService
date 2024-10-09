91fa989e71c544c5f4b9b6ab9f42b32ebb5b8ebe
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-09 12:11:29 +0500

    Message Service changed with library and more

--- src/main/java/uz/sb/messageservice/clients/BlockingServiceClient.java
+++ src/main/java/uz/sb/messageservice/clients/BlockingServiceClient.java
@@ -1,12 +1,10 @@
 package uz.sb.messageservice.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import uz.sb.messageservice.config.FeignConfig;
-import uz.sb.messageservice.domain.dto.request.BlockingRequest;
+
 
 @FeignClient(name = "AUTH-SERVICE", configuration = FeignConfig.class)
 public interface BlockingServiceClient {
